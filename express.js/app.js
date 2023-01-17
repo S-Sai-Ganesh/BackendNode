ae9246789f31f709300f35e5e68a666e526eb598
code-Fighter1 <ganeshsai29101@gmail.com>
2023-01-17 23:45:11 +0530

    routes in Express

--- express.js/app.js
+++ express.js/app.js
@@ -1,22 +1,23 @@
-// const http = require('http');
-
 const express = require('express');
+
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('From middleware1');
-    next();
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<h1>The add product page</h1><form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity" placeholder="quantity"><button type="submit">Add product</button></form>');
 })
 
-app.use((req,res,next)=>{
-    console.log('middleware2');
-    res.send({key1: 'value'})
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Default page</h1>')
 })
 
 
 app.listen('3000');
-// const server = http.createServer(app);
-
-// server.listen(3000);
-
