790e2aea1b77a83b19045185133a6c55e2a3a0a7
code-Fighter1 <ganeshsai29101@gmail.com>
2023-04-20 20:24:28 +0530

    using to mongoose

--- startingDynamicRoutes/app.js
+++ startingDynamicRoutes/app.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/User');
+// const User = require('./models/User');
 
 const app = express();
 
@@ -18,20 +18,24 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findUserById('643f652a33d565f5cec4690a')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.mobile, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findUserById('643f652a33d565f5cec4690a')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.mobile, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect('mongodb+srv://mongodbusername:OYYhMshMIuAnpIb8@cluster0.vtzjy7t.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(result=>{
+    app.listen(3000);
+    console.log('Connected!');
+  })
+  .catch(err=> console.log(err));
--- startingDynamicRoutes/controllers/shop.js
+++ startingDynamicRoutes/controllers/shop.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
@@ -28,7 +28,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('shop/index', {
         prods: products,
@@ -64,36 +64,6 @@ exports.postCart = (req, res, next) => {
     console.log(result);
     res.redirect('/cart');
   });
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user
-  //   .getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } });
-  //   })
-  //   .then(products => {
-  //     let product;
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findByPk(prodId);
-  //   })
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity }
-  //     });
-  //   })
-  //   .then(() => {
-  //     res.redirect('/cart');
-  //   })
-  //   .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
