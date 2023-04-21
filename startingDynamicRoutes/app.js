f4bd1d01e68b9ccecd05cf2165ae0988c2afc415
code-Fighter1 <ganeshsai29101@gmail.com>
2023-04-21 08:53:10 +0530

    user table and relations

--- startingDynamicRoutes/app.js
+++ startingDynamicRoutes/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/User');
+const User = require('./models/User');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findUserById('643f652a33d565f5cec4690a')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.mobile, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('6441f7e5650dc7c90d1cb1fe')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,6 +34,15 @@ app.use(errorController.get404);
 
 mongoose
   .connect('mongodb+srv://mongodbusername:OYYhMshMIuAnpIb8@cluster0.vtzjy7t.mongodb.net/shop?retryWrites=true&w=majority')
+  // .then(()=>{
+  //   return new User({
+  //     name: 'John',
+  //     email: 'john@test.com',
+  //     cart: {
+  //       items: []
+  //     }
+  //   }).save()
+  // })
   .then(result=>{
     app.listen(3000);
     console.log('Connected!');
